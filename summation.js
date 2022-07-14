0852ef58c66d8c023661cf974a87099887ddfed2
Lupio_Оля <skyuii389@gmail.com>
2022-07-15 00:46:32 +0300

    add summation

---
+++ summation.js
@@ -0,0 +1,8 @@
+//Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+var summation = function (num) {
+    let sum = 0;
+    for (let i = 0; i <= num; i++) {
+        sum += i;
+    }
+    return sum;
+}
